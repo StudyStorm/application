0f372ccbc4a9366c25589e77e9e190d31bc33656
Maxime Scharwath <maxscharwath@gmail.com>
2022-08-23 20:58:29 +0200

    chore: add i18n and animated logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 import { defineNuxtConfig } from "nuxt";
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@vueuse/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    "@nuxtjs/i18n",
+  ],
+  i18n: {
+    locales: [
+      { code: "fr", iso: "fr-FR", file: "fr-FR.yaml" },
+      { code: "en", iso: "en-US", file: "en-US.yaml" },
+    ],
+    langDir: "locales/",
+    defaultLocale: "fr",
+    vueI18n: {
+      fallbackLocale: "fr",
+    },
+  },
 });
