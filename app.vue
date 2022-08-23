c2b67b666327d411bea271a183c49427fc8048fe
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-08-23 15:18:56 +0200

    feat: wip landing page

--- app.vue
+++ app.vue
@@ -1,11 +1,9 @@
-<script setup>
-const title = "Hello World";
-console.log(title);
-</script>
+<script setup></script>
 
 <template>
-  <div>
-    <h1 class="font-bold text-blue-600">{{ title }}</h1>
-    <SCard />
-  </div>
+   <div>
+      <NuxtLayout>
+         <NuxtPage />
+      </NuxtLayout>
+   </div>
 </template>
