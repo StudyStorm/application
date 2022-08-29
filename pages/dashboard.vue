6439ab4e64d2fd20f3695bb6ce1f7ece825f5cab
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-08-29 14:43:14 +0200

    feat: Created base layouts

---
+++ pages/dashboard.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div>Dashboard</div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,9 @@
+<script setup>
+definePageMeta({
+  layout: "nosidebar",
+});
+</script>
+
+<template>
+  <div>Login view <BeakerIcon class="w-6 h-6 text-blue-500" /></div>
+</template>
