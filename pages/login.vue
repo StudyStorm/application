089c98f08e8d77bacc86d70f7ec70a6b51a31564
Marinlestylo <chil.tfm@gmail.com>
2022-08-30 10:52:50 +0200

    feat: Login page is now fully functionnal
    The submit  button now sends data to the backend and redirect the user if the connection is successful. It show an error message if the connection failed.

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 const img = '/images/post-it-for-login.png'
+const router = useRouter();
 const credentials = ref({
   email: '',
   password: ''
 })
+const err = ref<null | any>(null);
 async function login() {
-
+  const { data: answer, error } = await useFetchAPI('/login', {
+    method: 'POST',
+    body: (credentials.value),
+    initialCache: false
+  });
+  err.value = !answer.value;
+  if (answer.value) {
+    router.push('/');
+  }
 }
 </script>
 
@@ -23,9 +33,20 @@ async function login() {
             {{  $t("app.login.title")  }}
           </h2>
         </div>
+        <div v-if="err" class="relative px-4 py-3 mt-6 text-red-700 bg-red-100 border border-red-400 rounded" role="alert">
+          <span class="block sm:inline">{{ $t("app.login.errorLogin")}}</span>
+          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+            <svg class="w-6 h-6 text-red-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20">
+              <title>Close</title>
+              <path
+                d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
+          </span>
+        </div>
 
-        <div class="mt-8">
-          <div class="mt-6">
+        <div>
+          <div class="mt-2">
             <form @submit.prevent="login" class="space-y-6">
               <div>
                 <label for="email" class="block text-sm font-medium text-gray-700"> {{  $t("app.login.email")  }} </label>
@@ -61,7 +82,7 @@ async function login() {
             </form>
             <div class="flex items-center justify-start mt-6">
               <div class="text-sm">
-                <a href="#" class="font-medium text-black hover:text-storm-blue"> {{  $t("app.login.noAccount")  }} </a>
+                <a href="/register" class="font-medium text-black hover:text-storm-blue"> {{  $t("app.login.noAccount")  }} </a>
               </div>
             </div>
           </div>
