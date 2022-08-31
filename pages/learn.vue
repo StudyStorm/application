226a1f60e9b1e94793036a5b15920ff4d8576eb0
Alen Bijelic <alen.bijelic@heig-vd.ch>
2022-08-31 17:12:27 +0200

    feat: add modal to learning page

--- pages/learn.vue
+++ pages/learn.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-   import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/24/outline/index.js";
+   import { ChevronLeftIcon, ChevronRightIcon, FlagIcon } from "@heroicons/vue/24/outline/index.js";
 
    const err = ref<null | any>(null);
    const showModal = ref(false);
@@ -9,18 +9,20 @@
       // TODO: Signal current card
       showModal.value = false;
    }
+
+
 </script>
 
 <template>
    <div
     class="border-b border-gray-200 p-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
   >
-    <div class="min-w-0">
-      <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
-        Decks name
-      </h1>
-    </div>
-  </div>
+      <div class="min-w-0">
+         <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
+            Decks name
+         </h1>
+      </div>
+   </div>
   <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
     <div class="bg-white px-4 py-8 sm:rounded-lg sm:px-10">
          <FlashCard></FlashCard>
@@ -36,12 +38,25 @@
          </div>
     </div>
   </div>
+  <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div class="bg-white px-4 py-8 sm:rounded-lg sm:px-10 space-y-6">
+      <div>
+         <button
+         type="button"
+         class="flex w-full justify-center rounded-md bg-gray-100 px-4 py-2 text-sm font-medium text-red-600 shadow-sm hover:bg-grey-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+         @click="showModal = true"
+         >
+         {{ $t("app.learn.buttons.signal") }}
+         </button>
+      </div>
+    </div>
+  </div>
   <Modal v-model="showModal">
     <template #icon
       ><div
         class="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
       >
-        <ExclamationTriangleIcon
+        <FlagIcon
           class="h-6 w-6 text-red-600"
           aria-hidden="true"
         />
@@ -50,25 +65,28 @@
     <template #title>
       {{ $t("app.learn.modal.title") }}
     </template>
-    <template #content>
-      {{ $t("app.learn.modal.content") }}
-    </template>
-    <template #footer>
+   <template #content>
+      <div class="mb-6">
+         <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ $t("app.learn.modal.message") }}</label>
+         <input type="text" id="message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+      </div>
+   </template>
+   <template #footer>
       <button
-        type="button"
-        class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-        @click="showModal = false"
+      type="button"
+      class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+      @click="signalCard"
       >
-        {{ $t("app.learn.modal.buttons.confirmSignal") }}
+      {{ $t("app.learn.modal.buttons.confirmSignal") }}
       </button>
       <button
-        ref="cancelButtonRef"
-        type="button"
-        class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-        @click="showModal = false"
+      ref="cancelButtonRef"
+      type="button"
+      class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+      @click="showModal = false"
       >
-        {{ $t("app.profile.modal.buttons.cancel") }}
+      {{ $t("app.profile.modal.buttons.cancel") }}
       </button>
-    </template>
+   </template>
   </Modal>
 </template>
