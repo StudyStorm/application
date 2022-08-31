1899890820ed30c2101e2dd3fceb71fc262896b1
Marinlestylo <chil.tfm@gmail.com>
2022-08-31 13:20:54 +0200

    fix: correct the color of the email field

--- pages/resetPassword.vue
+++ pages/resetPassword.vue
@@ -43,7 +43,7 @@ definePageMeta({
                         <div class="block mb-2 text-sm text-gray-900">
                             {{  $t('app.reset.email')  }}
                         </div>
-                        <input type="email" disabled value="marinelepen@gmail.com" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
+                        <input type="email" disabled value="marinelepen@gmail.com" class="bg-gray-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
                     </div>
                     <div>
                         <label for="password" class="block mb-2 text-sm text-gray-900">{{  $t('app.reset.password') }}</label>
