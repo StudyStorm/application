5af19439269ac9979b8126d0b4d174afe3dd09b2
Alen Bijelic <alen.bijelic@heig-vd.ch>
2022-08-31 09:12:01 +0200

    feat: add delete account modal

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+// import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline";
+
+const openModal = ref(false);
+
 const userInformation = ref({
   firstName: "",
   lastName: "",
@@ -27,6 +38,11 @@ async function save() {
       successfullSave = true;
     }
   }
+}
+
+async function deleteAccount() {
+  // TODO: Delete user account through backend
+  openModal.value = false;
 }
 </script>
 
@@ -81,7 +97,6 @@ async function save() {
               v-model="userInformation.lastName"
               name="lastname"
               type="text"
-              autocomplete="lastname"
               required
               class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             />
@@ -148,6 +163,7 @@ async function save() {
         <div>
           <button
             class="bg-storm-red hover:bg-storm-red-hover flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            @click="openModal = true"
           >
             {{ $t("app.profile.buttons.delete") }}
           </button>
@@ -155,4 +171,86 @@ async function save() {
       </form>
     </div>
   </div>
+  <TransitionRoot as="template" :show="openModal">
+    <Dialog as="div" class="relative z-10" @close="openModal = false">
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+        />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div
+          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <DialogPanel
+              class="relative overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+            >
+              <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div class="sm:flex sm:items-start">
+                  <div
+                    class="mx-auto flex h-12 w-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+                  >
+                    <!--<ExclamationTriangleIcon
+                      class="h-6 w-6 text-red-600"
+                      aria-hidden="true"
+                    />-->
+                  </div>
+                  <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <DialogTitle
+                      as="h3"
+                      class="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      {{ $t("app.profile.modal.title") }}
+                    </DialogTitle>
+                    <div class="mt-2">
+                      <p class="text-sm text-gray-500">
+                        {{ $t("app.profile.modal.content") }}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"
+              >
+                <button
+                  type="confirmDeleteRef"
+                  class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                  @click="deleteAccount"
+                >
+                  {{ $t("app.profile.modal.buttons.confirmDelete") }}
+                </button>
+                <button
+                  ref="cancelButtonRef"
+                  type="button"
+                  class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  @click="openModal = false"
+                >
+                  {{ $t("app.profile.modal.buttons.cancel") }}
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
