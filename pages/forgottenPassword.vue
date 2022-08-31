f39a82188ccf351a6833b2dce760d1449630b273
Marinlestylo <chil.tfm@gmail.com>
2022-08-31 14:33:16 +0200

    feat: data validation on the front

--- pages/forgottenPassword.vue
+++ pages/forgottenPassword.vue
@@ -22,7 +22,7 @@ function sendEmail() {
                 <form @submit.prevent="sendEmail">
                     <label for="email" class="block mb-2 text-sm text-gray-900">{{  $t('app.forgottenPassword.email') 
                         }}</label>
-                    <input type="email" name="email" id="email" placeholder="John.doe@gmail.com"
+                    <input type="email" name="email" id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                         required>
                     <button type="submit"
