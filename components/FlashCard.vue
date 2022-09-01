dfda08b19bcc54478334b54a5ac955bdfd77140c
Alen Bijelic <alen.bijelic@heig-vd.ch>
2022-09-01 14:56:11 +0200

    feat: add progress bar and fix card navigation

--- components/FlashCard.vue
+++ components/FlashCard.vue
@@ -2,31 +2,49 @@
   import { FlagIcon } from "@heroicons/vue/24/outline/index.js";
 
    const isFlipped = ref(false)
+   const card = ref()
+   const flippedCard = ref()
 
-    defineProps({
+    const props = defineProps({
       question: String,
       response: String
     });
 
-    const emit = defineEmits(['update:card'])
-
-    const updateValues = (event) => {
-        emit('update:card', event.target.value)
+    async function sleep(ms) {
+      return await new Promise(resolve => setTimeout(resolve, ms));
     }
+
+    async function resetCard(){
+      if (isFlipped.value){
+        flippedCard.value.style.transform = 'rotateY(180deg)';
+        isFlipped.value = false;
+        await sleep(300);
+      }
+    }
+
+    defineExpose({
+      resetCard
+    });
+
 </script>
 
 <template>
   <div class="container">
       <div
+        ref="card"
         class="card"
         @click="isFlipped = !isFlipped"
         v-bind:class="{ flipCard: isFlipped }"
       >
-        <div class="cardFace">
-          {{ question }}
+        <div class="cardFace flex items-center">
+          <span class="cardText">
+            {{ question }}
+          </span>
         </div>
-        <div class="cardFace flipCardBack">
-          {{ response }}
+        <div class="cardFace flipCardBack flex items-center" ref="flippedCard">
+          <span class="cardText">
+            {{ response }}
+          </span>
         </div>
       </div>
     </div>
@@ -53,13 +71,17 @@
     position: absolute;
     width: 100%;
     height: 100%;
-    line-height: 400px;
+    backface-visibility: hidden;
+    background: #F2F2F2;
+  }
+
+  .cardText {
     color: #3F434A;
     text-align: center;
     font-weight: bold;
-    font-size: 40px;
-    backface-visibility: hidden;
-    background: #F2F2F2;
+    font-size: 22px;
+    margin-left:auto;
+    margin-right:auto;
   }
   
   .flipCard {
