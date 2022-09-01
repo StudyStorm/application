b1d2cb97dc1005312c1cefa3ebe7ecc8fe0c2846
Marinlestylo <chil.tfm@gmail.com>
2022-09-01 10:48:44 +0200

    feat: Create classroom button for mobile (no logic)

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -122,6 +122,13 @@ const sidebarOpen = ref(false);
                       />
                       {{ item.name }}
                     </NuxtLink>
+                    <div class="flex justify-center">
+                      <button
+                        class="mt-4 flex justify-center rounded-md border border-transparent bg-indigo-700 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-storm-darkblue focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      >
+                        Create classroom
+                      </button>
+                    </div>
                   </div>
                 </nav>
               </div>
