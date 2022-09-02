52a056593705e027cd92232a7c821a3a8cb471ad
AlenBijelic99 <55805154+AlenBijelic99@users.noreply.github.com>
2022-09-02 16:58:40 +0200

    Fb/Learn page display cards (#42)
    * feat: WIP create FlashCard component with learning page
    
    * feat: add modal to learning page
    
    * feat: WIP navigate through questions
    
    * feat: add progress bar and fix card navigation
    
    * feat: card progress bar
    
    * feat: create custom Card component
    
    * feat: use checkbox for OptionsCard
    
    * feat: display right answers
    
    * fix: match backend interface with one custom attribut
    
    * refactor: remove useless code and change card format
    
    * feat: get needed information to signal a card
    
    Co-authored-by: Lazar Pavicevic <43219964+Lazzzer@users.noreply.github.com>

---
+++ components/FlashCard.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+   const card = ref()
+
+    const props = defineProps({
+      question: String,
+      answer: String
+    });
+
+
+    async function sleep(ms) {
+      return await new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function resetCard(){
+      await card.value.resetCard();
+    }
+
+    defineExpose({
+      resetCard
+    });
+
+</script>
+
+<template>
+  <Card ref="card" :isFlippable="true">
+    <template #cardFront>
+      <span class="cardText">
+        {{ question }}
+      </span>
+    </template>
+    <template #cardBack>
+      <span class="cardText">
+        {{ answer }}
+      </span>
+    </template>
+  </Card>
+</template>
+
+<style scoped>
+  .cardFace {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    background: #F2F2F2;
+  }
+
+  .cardText {
+    color: #3F434A;
+    text-align: center;
+    font-weight: bold;
+    font-size: 22px;
+    margin-left:auto;
+    margin-right:auto;
+  }
+</style>
---
+++ components/OptionsCard.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+    import { PropType } from 'vue'
+
+   const card = ref()
+   const inputs = ref([])
+   const displayRightAnswers = ref(false);
+
+    const props = defineProps({
+      question: String,
+      answers: Array as PropType<String[]>,
+      isTheAnswer: Array as PropType<Boolean[]>,
+    });
+
+    for (let i = 0; i < props.answers.length; ++i){
+        inputs.value.push(ref('input' + (i + 1)));
+    }
+
+    async function resetCard(){
+      await card.value.resetCard();
+    }
+
+    defineExpose({
+      resetCard
+    });
+</script>
+
+<template>
+  <Card ref="card" :isFlippable="false">
+    <template #cardFront>
+      <div class="grid grid-rows-1 space-y-6 items-center" style="margin-left: auto; margin-right: auto;">
+        <div class="text-center font-medium ">
+          <h1 class="text-gray-600 ">
+            {{ question }}
+          </h1>
+        </div>
+        <div>
+          <ul class="grid gap-2 w-full md:grid-cols-2">
+            <li v-for="i in answers.length">
+                <input :disabled="displayRightAnswers" type="checkbox" :ref="inputs[i]" :key="i" :id="'input' + i" :value="answers[i - 1]" class="hidden peer">
+                <label :for="'input' + i" v-bind:class="[{'peer-checked:border-purple-600': !displayRightAnswers}, {'peer-checked:border-green-600': displayRightAnswers && isTheAnswer[i - 1]}, {'bg-green-200 hover:bg-green-200 border-red-600': displayRightAnswers && isTheAnswer[i - 1]}, {'hover:bg-gray-200': !displayRightAnswers}, {'peer-checked:border-red-600': displayRightAnswers && !isTheAnswer[i - 1]}]" class="inline-flex items-center justify-center p-3 w-full text-gray-500 bg-white rounded-lg border-2 border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">                           
+                    <div class="block">
+                        <div class="w-full text-sm">{{ answers[i - 1] }}</div>
+                    </div>
+                </label>
+            </li>
+          </ul>
+        </div>
+        <div class="w-40" style="margin-left: auto; margin-right: auto;">
+          <button
+            type="button"
+            @click="displayRightAnswers = true"
+            class="flex w-full justify-center rounded-md border border-transparent bg-indigo-700 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-storm-darkblue focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            {{ $t("app.learn.buttons.checkAnswer") }}
+          </button>
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
+
+<style scoped>
+  .cardText {
+    text-align: center;
+    font-weight: bold;
+    font-size: 22px;
+    margin-left:auto;
+    margin-right:auto;
+  }
+</style>
