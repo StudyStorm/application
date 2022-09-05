46284ee127918ca8cf84ecb74f949b1672b5c9db
Lazzzer <lazazr@gmail.com>
2022-09-05 21:56:46 +0200

    feat: working profile page (but still buggy)

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,29 +1,26 @@
 <script setup lang="ts">
 import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline/index.js";
-// import { useProfileStore } from "../store/profile";
+import { FormError } from "~/types/app";
+import { useProfileStore } from "~/store/profile";
 
-const userInformation = ref(useNuxtApp().$auth.user);
-const confirmPassword = ref("");
-// const err = ref<null | any>(null);
 const showModal = ref(false);
 
-// const profileStore = useProfileStore();
+const errors = ref<FormError | null>(null);
+
+const profileStore = useProfileStore();
+const userInformation = profileStore.profile;
 
 const updatePicture = (picture: File) => {
   console.log("Save got", picture);
 };
 
 async function save() {
-  if (userInformation.value.password === confirmPassword.value) {
-    // profileStore.updateProfile(userInformation.value);
-    // await profileStore.updateProfile(userInformation.value);
-    // err.value = !data.value;
-  }
+  await profileStore.updateProfile();
 }
 
 async function deleteAccount() {
-  showModal.value = true;
-  // await profileStore.deleteProfile();
+  showModal.value = false;
+  await profileStore.deleteProfile();
 }
 </script>
 
@@ -38,58 +35,58 @@ async function deleteAccount() {
         </h1>
       </div>
     </div>
-    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div class="bg-white px-4 py-8 sm:rounded-lg sm:px-10">
-        <form class="space-y-6" @submit.prevent="save">
-          <div class="flex">
-            <div class="mx-auto text-center">
-              <div class="mb-4">
-                <label class="mt-6 cursor-pointer">
-                  <s-file-input-preview
-                    class="mt-6 cursor-pointer"
-                    accept="image/png, image/jpeg, image/jpg, image/gif"
-                    @valid-file="updatePicture"
-                  />
-                </label>
-              </div>
-            </div>
+    <div class="sm:mx-auto sm:w-full sm:max-w-md lg:mt-8">
+      <div class="flex justify-center px-4 pb-2 sm:rounded-lg sm:px-10">
+        <s-form
+          class="w-full max-w-sm space-y-2 px-7 pb-4 md:space-y-6"
+          :errors="errors"
+          @submit.prevent=""
+        >
+          <div class="mx-auto h-24 w-24 text-center">
+            <s-file-input-preview
+              class="mt-6 cursor-pointer"
+              accept="image/png, image/jpeg, image/jpg, image/gif"
+              :url="userInformation.picture_url"
+              @valid-file="updatePicture"
+            />
           </div>
 
           <div>
             <label
-              for="first_name"
+              for="firstName"
               class="block text-sm font-medium text-gray-700"
             >
               {{ $t("app.profile.labels.firstname") }}
             </label>
             <div class="mt-1">
-              <input
-                id="first_name"
+              <s-input
+                id="firstName"
                 v-model="userInformation.first_name"
-                name="first_name"
+                name="firstName"
                 type="text"
-                autocomplete="first_name"
                 required
-                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                autocomplete="given-name"
+                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-storm-blue focus:outline-none focus:ring-storm-blue sm:text-sm"
               />
             </div>
           </div>
 
           <div>
             <label
-              for="last_name"
+              for="lastName"
               class="block text-sm font-medium text-gray-700"
             >
               {{ $t("app.profile.labels.lastname") }}
             </label>
             <div class="mt-1">
-              <input
-                id="last_name"
+              <s-input
+                id="lastName"
                 v-model="userInformation.last_name"
-                name="last_name"
+                name="lastName"
                 type="text"
+                autocomplete="family-name"
                 required
-                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-storm-blue focus:outline-none focus:ring-storm-blue sm:text-sm"
               />
             </div>
           </div>
@@ -98,14 +95,14 @@ async function deleteAccount() {
               {{ $t("app.profile.labels.email") }}
             </label>
             <div class="mt-1">
-              <input
+              <s-input
                 id="email"
                 v-model="userInformation.email"
                 name="email"
                 type="email"
                 autocomplete="email"
                 required
-                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-storm-blue focus:outline-none focus:ring-storm-blue sm:text-sm"
               />
             </div>
           </div>
@@ -118,55 +115,46 @@ async function deleteAccount() {
               {{ $t("app.profile.labels.password") }}
             </label>
             <div class="mt-1">
-              <input
+              <s-password-input
                 id="password"
                 v-model="userInformation.password"
                 name="password"
                 type="password"
                 autocomplete="current-password"
-                required
-                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-storm-blue focus:outline-none focus:ring-storm-blue sm:text-sm"
               />
             </div>
           </div>
-
-          <div>
-            <label
-              for="password"
-              class="block text-sm font-medium text-gray-700"
-            >
-              {{ $t("app.profile.labels.confirmPassword") }}
-            </label>
-            <div class="mt-1">
-              <input
-                id="confirm_password"
-                v-model="confirmPassword"
-                name="confirm_password"
-                type="password"
-                autocomplete="curent-password"
-                required
-                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 shadow-sm placeholder:text-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-              />
-            </div>
-          </div>
-          <div>
+          <div class="pt-2">
             <button
               type="submit"
-              class="flex w-full justify-center rounded-md border border-transparent bg-indigo-700 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-storm-darkblue focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              class="flex w-full justify-center rounded-md border border-transparent bg-storm-blue py-3 text-base font-medium text-white shadow-sm hover:bg-storm-darkblue focus:outline-none focus:ring-2 focus:ring-storm-blue focus:ring-offset-2 md:py-2 md:text-sm"
+              @click="save"
             >
               {{ $t("app.profile.buttons.save") }}
             </button>
           </div>
-          <div>
-            <button
-              type="button"
-              class="flex w-full justify-center rounded-md border border-transparent bg-storm-red px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-storm-red-hover focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              @click="deleteAccount"
-            >
-              {{ $t("app.profile.buttons.delete") }}
-            </button>
+          <div class="pt-2">
+            <div class="rounded-md border border-red-400 bg-red-200 p-4">
+              <div class="mx-2 mb-2 -mt-2 flex items-center">
+                <p class="font-sm mr-1 font-medium text-red-600">
+                  {{ $t("app.profile.buttons.danger") }}
+                </p>
+                <ExclamationTriangleIcon
+                  class="inline-block h-5 text-red-600"
+                  aria-hidden="true"
+                />
+              </div>
+              <button
+                type="submit"
+                class="flex w-full justify-center rounded-md border border-transparent bg-storm-red py-3 text-base font-medium text-white shadow-sm hover:bg-storm-red-hover focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-red-200 md:py-2 md:text-sm"
+                @click="showModal = true"
+              >
+                {{ $t("app.profile.buttons.delete") }}
+              </button>
+            </div>
           </div>
-        </form>
+        </s-form>
       </div>
     </div>
 
@@ -190,13 +178,13 @@ async function deleteAccount() {
       <template #footer>
         <button
           class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-          @click="showModal = false"
+          @click="deleteAccount()"
         >
           {{ $t("app.profile.modal.buttons.confirmDelete") }}
         </button>
         <button
           type="button"
-          class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-storm-darkblue focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
           @click="showModal = false"
         >
           {{ $t("app.profile.modal.buttons.cancel") }}
--- store/profile.ts
+++ store/profile.ts
@@ -1,13 +1,41 @@
 import { defineStore } from "pinia";
+import { FormError } from "~/types/app";
 
 export const useProfileStore = defineStore("profile", {
-  state: () => ({}),
+  state: () => ({
+    profile: { ...useNuxtApp().$auth.user },
+  }),
   actions: {
     async updateProfile() {
-      // TODO +  update picture
+      const config = useRuntimeConfig();
+      const { id, created_at, ...payload } = this.profile;
+      const { error } = await useFetch("/v1/profile", {
+        method: "PATCH",
+        body: payload,
+        credentials: "include",
+        baseURL: config.apiURL,
+        initialCache: false,
+      });
+
+      if (error.value) {
+        console.log(error.value);
+        return error;
+      } else {
+        this.profile.password = null;
+        await useNuxtApp().$auth.fetchUser();
+      }
     },
     async deleteProfile() {
-      // TODO: delete
+      const { error } = await useFetchAPI("/v1/profile", {
+        method: "DELETE",
+      });
+      console.log(error);
+
+      if (error) {
+        return error;
+      } else {
+        useNuxtApp().$auth.logout();
+      }
     },
   },
 });
