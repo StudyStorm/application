dd8d1d43e9f1c417b8731b2aa6d97524ca3aa7be
Jonathan <chil.tfm@gmail.com>
2022-09-05 14:53:25 +0200

    fix: linting

--- components/SLangSwitcher.vue
+++ components/SLangSwitcher.vue
@@ -2,11 +2,11 @@
 const i18n = useI18n();
 </script>
 <template>
-    <slot
-      v-for="locale in $i18n.locales"
-      :key="locale.code"
-      :locale="locale"
-      :on-click="() => i18n.setLocale(locale.code)"
-      :active="i18n.locale.value === locale.code"
-    />
+  <slot
+    v-for="locale in $i18n.locales"
+    :key="locale.code"
+    :locale="locale"
+    :on-click="() => i18n.setLocale(locale.code)"
+    :active="i18n.locale.value === locale.code"
+  />
 </template>
