bf8b2c74fa4ae6cff49f52248ac80a1f83e6698a
Marinlestylo <chil.tfm@gmail.com>
2022-09-06 15:53:40 +0200

    fix: use component on the landing page

--- components/landing/LandingNavbar.vue
+++ components/landing/LandingNavbar.vue
@@ -118,53 +118,7 @@ onClickOutside(target, () => (showMobileMenu.value = false));
           <li
             class="block rounded-lg py-2 pl-3 pr-4 text-storm-dark hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-gray-200 lg:bg-transparent lg:p-0 lg:focus:ring-0"
           >
-            <Menu as="div" class="relative inline-block text-left">
-              <div>
-                <MenuButton>
-                  <GlobeAltIcon class="h-6 w-6 text-storm-dark" />
-                </MenuButton>
-              </div>
-
-              <transition
-                enter-active-class="transition duration-100 ease-out"
-                enter-from-class="transform scale-95 opacity-0"
-                enter-to-class="transform scale-100 opacity-100"
-                leave-active-class="transition duration-75 ease-in"
-                leave-from-class="transform scale-100 opacity-100"
-                leave-to-class="transform scale-95 opacity-0"
-              >
-                <MenuItems
-                  class="absolute -right-16 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                >
-                  <div class="py-1">
-                    <s-lang-switcher
-                      v-slot="{ locale, active, tabindex, onClick }"
-                      class=""
-                    >
-                      <MenuItem>
-                        <span
-                          :class="[
-                            active ? 'font-bold' : '',
-                            'flex justify-start items-center px-2 py-2 text-sm text-storm hover:bg-gray-100 hover:text-gray-900 text-gray-700',
-                          ]"
-                          @click="onClick"
-                        >
-                          <div class="h-6 w-6">
-                            <NuxtImg
-                              :src="langIconLocation + country[tabindex]"
-                              class="h-6 w-6 rounded-full"
-                            />
-                          </div>
-                          <div class="ml-2">
-                            {{ locale.name }}
-                          </div>
-                        </span>
-                      </MenuItem>
-                    </s-lang-switcher>
-                  </div>
-                </MenuItems>
-              </transition>
-            </Menu>
+            <SButtonLangSwitch></SButtonLangSwitch>
           </li>
           <!-- <s-lang-switcher
                 v-slot="{ locale, active, onClick }"
