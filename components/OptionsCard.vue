1f690f0b6d26f08ebc8c497dc36ec758855c3a6d
Maxime Scharwath <maxscharwath@gmail.com>
2022-09-07 21:55:27 +0200

    fix click card flip

--- components/OptionsCard.vue
+++ components/OptionsCard.vue
@@ -48,7 +48,7 @@ defineExpose({
         </h1>
       </div>
       <ul class="grid w-full gap-2 md:grid-cols-2">
-        <li v-for="(answer, i) in answers" :key="i">
+        <li v-for="(answer, i) in answers" :key="i" class="">
           <input
             :id="`input${i}`"
             :disabled="displayRightAnswers"
@@ -58,15 +58,13 @@ defineExpose({
           <label
             :for="`input${i}`"
             :class="answerStyle(answer.isTheAnswer)"
-            class="inline-flex w-full cursor-pointer items-center justify-center rounded-lg border-2 border-gray-200 bg-white p-3 text-gray-500 hover:text-gray-600 peer-checked:text-gray-600 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:peer-checked:text-gray-300"
+            class="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg border-2 border-gray-200 bg-white p-3 text-gray-500 hover:text-gray-600 peer-checked:text-gray-600 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:peer-checked:text-gray-300"
           >
-            <div class="block">
-              <div class="w-full text-sm">{{ answer.label }}</div>
-            </div>
+            <span class="block w-full text-sm">{{ answer.label }}</span>
           </label>
         </li>
       </ul>
-      <div class="w-40" style="margin-left: auto; margin-right: auto">
+      <div class="my-2 w-40" style="margin-left: auto; margin-right: auto">
         <button
           type="button"
           class="flex w-full justify-center rounded-md border border-transparent bg-indigo-700 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-storm-darkblue focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
