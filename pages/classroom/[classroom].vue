29ee20664a0c5eed2294e23034c966ab366cf348
Marinlestylo <chil.tfm@gmail.com>
2022-09-07 22:34:38 +0200

    fix: on the create folder modal the input has the same size than others

--- pages/classroom/[classroom].vue
+++ pages/classroom/[classroom].vue
@@ -177,7 +177,7 @@ await classroomStore.fetchClassroomUsers(route.params.classroom as string);
       <template #title> {{ $t("app.classroom.folderModal.title") }} </template>
       <template #content>
         <s-form
-          class="w-full max-w-sm space-y-2 px-7"
+          class="w-full space-y-2 sm:max-w-sm"
           :errors="errors"
           @submit.prevent="createFolder"
         >
@@ -192,6 +192,7 @@ await classroomStore.fetchClassroomUsers(route.params.classroom as string);
               type="text"
               required
               autocomplete="off"
+              placeholder="Folder name"
               class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-storm-dark shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             />
           </div>
