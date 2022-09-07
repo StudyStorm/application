8a5e0277124a2ff28a1da33595162d2725c834c6
Nicolas Crausaz <n.crausaz99@gmail.com>
2022-09-07 21:16:25 +0200

    delete classroom + decks ok

--- components/UpdateDeckModal.vue
+++ components/UpdateDeckModal.vue
@@ -3,7 +3,6 @@ import { ref } from "#imports";
 import { useDecksStore } from "~/store/decks";
 import { SquaresPlusIcon } from "@heroicons/vue/24/outline/index.js";
 const store = useDecksStore();
-const router = useRouter();
 
 const showModal = ref(false);
 
@@ -16,12 +15,6 @@ const init = () => {
 const updateDeck = async () => {
   await store.updateDeckName(store.currentDeck.id, editingDeckName.value);
   showModal.value = false;
-};
-
-const deleteDeck = async () => {
-  await store.deleteDeck(store.currentDeck.id);
-  showModal.value = false;
-  router.push({ name: "dashboard" });
 };
 </script>
 
@@ -68,13 +61,6 @@ const deleteDeck = async () => {
         @click="showModal = false"
       >
         {{ $t("app.deck.modal.buttons.cancel") }}
-      </button>
-      <button
-        type="button"
-        class="float-left mt-3 inline-flex w-full justify-center rounded-md border border-storm-red bg-storm-red px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-        @click="deleteDeck"
-      >
-        {{ $t("app.deck.modal.buttons.delete") }}
       </button>
     </template>
   </Modal>
